487e46cb1ad0c15ea6a634498880d9ca98f10c2a
“Alexandre <coma.alexandre@gmail.com>
2022-06-28 13:00:41 +0200

    implemented the creation of cards twice, the creation of IDs for the same cards, the array of object for the cards. Working on the matching method

--- js/main.js
+++ js/main.js
@@ -1,22 +1,29 @@
 
 const arrayOfCards = []; // store somewere else
-const arrayOfImages = ["Images/belgium.png","Images/france.png","Images/germany.png","Images/germany.png","Images/italy.png","Images/netherlands.png","Images/portugal.png","Images/spain.png","Images/united-kingdom.png"]; // store somewere else
+const arrayOfImages = [
+        {Id: "Belgium", img:"Images/belgium.png"},
+        {Id: "France", img:"Images/france.png"},
+        {Id: "Germany", img:"Images/germany.png"},
+        {Id: "Italy",img:"Images/italy.png"},
+        {Id: "Netherlands", img:"Images/netherlands.png"},
+        {Id: "Portugal", img:"Images/portugal.png"},
+        {Id: "Spain", img:"Images/spain.png"},
+        {Id: "UK", img:"Images/united-kingdom.png"}]; // store somewere else
 
 
 class Game {
     constructor() {
-        
+        this.pairs = [];
     }
     start(){
-        const card = new Card();
-        card.createDomElement();
+        const card1 = new Card();
+        const card2 = new Card();
         console.log("starting the game");
 
 
       let counter = 5;
       const intervalId = setInterval(function () {
             counter--;
-            //console.log(counter)
             if (counter === 0){
                 const gameOverMessage = document.createElement("div");
                 gameOverMessage.className = "game-over";
@@ -30,27 +37,22 @@ class Game {
                 clearInterval(intervalId);
             }*/
       },1000)
-      
-
-      
-
     }
-
-    flipCard() {
-        const cardId = document.getElementsByTagName("img");
-        cardId.addEventListener("click", flipCard);
-        cardId.classList.toggle("flipCard");
-      };
     
-      // need to hide/anonymize the src or ids otherwise we can cheat with the devtools
-    matchCards() {
-        if(frontCard.src === frontCard.src) {
+      // need to hide/anonymize otherwise we can cheat with the devtools
+    matchCards(card1, card2) {
+        for (let i=0; i<clickedCards.length;i++){
+            if( clickedCards[i] === clickedCards[i+1]) {
+                this.pairs++
+            }
+            else {
+                card1.backcard.style.display = block;
+                card2.backcard.style.display = block;
+            };
+        };
+        console.log(this.pairs)
+    };
     
-        }
-        else {
-    
-        }
-      };
       // Display timer
 
       // Next level
@@ -60,63 +62,81 @@ class Game {
   
   
   class Card {
-    constructor (ID) {
-        this.Id = ID;
-        this.height = 10;
-        this.width = 5;
+    constructor () {
         this.domElement = this.createDomElement();
     }
 
   
     createDomElement() {
         let cardsNumber = 8;
-       
         for (let i=0; i<cardsNumber ; i++) {
-       
+        
         //Create card div
-        let randomNumber = Math.round(Math.random()*arrayOfImages.length);
         const newElm = document.createElement('div');
-        newElm.Id = this.Id;
         newElm.className = "card";
         
         // create back image
         const backCard = document.createElement('img');
         backCard.className = "card--image--back";
-        
         backCard.src = "Images/question-mark.png" ; // always the same cover
-        /*backCard.addEventListener('click', function() {
-            backCard.style.display = "none";
-            frontCard.style.display = "block";
-        })*/
         newElm.appendChild(backCard);
 
         // create front card
         const frontCard = document.createElement('img');
         frontCard.className = "card--image--front";
-        frontCard.src = `${arrayOfImages[randomNumber]}`;
+        frontCard.src = arrayOfImages[i]["img"];
+        newElm.setAttribute("id",arrayOfImages[i]["Id"]);
+        
+        /* picks a random image from the array without repeating
+        frontCard.src = "";
+        function noRepeat(arrayOfImages) {
+            let copy = arrayOfImages.slice(0);
+            console.log(copy)
+            return function () {
+                if(copy.length < 1) { copy = arrayOfImages.slice(0)}
+                let index = Math.floor(Math.random() * copy.length);
+                let item = copy[index];
+                copy.slice(index, 1);
+                return item;
+            };
+        }
+        const chooser = noRepeat(arrayOfImages)
+        frontCard.src = chooser();*/
+
         newElm.appendChild(frontCard);
 
-        //click
+        // flip the cards on click
         newElm.addEventListener( 'click', function() {
-            console.log(backCard)
             if(backCard.style.display == "block"){
                 backCard.style.display = "none";
                 frontCard.style.display = "block";
-                console.log("click inside if")
             }
             else {
                 backCard.style.display = "block";
                 frontCard.style.display = "none";
             }
+       
+        const clickedCards = [];
+        clickedCards.push(newElm.getAttribute("Id"));
+        function matchCards(card1, card2) {
+            for (let i=0; i<clickedCards.length;i++){
+                if( clickedCards[i] === clickedCards[i+1]) {
+                    this.pairs++
+                }
+                else {
+                    card1.backcard.style.display = block;
+                    card2.backcard.style.display = block;
+                };
+            };
+            console.log(this.pairs)
+        };
         });
         
         //append new div with the back and front to the board
         const boardElm = document.getElementById("board"); 
         boardElm.appendChild(newElm);
-        // boardElm.appendChild(newElm.clone());  // need to create the same card twice
   
           // need to push the two cards created at random positions in the array
-        arrayOfCards.push()
         }
     }
     
